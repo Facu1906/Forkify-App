3979799dddfa42c967946ff72e09457a993c80d0
Facundo Franco <132781893+Facu1906@users.noreply.github.com>
2023-08-30 11:08:57 -0300

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f95e1417-f9cc-4eb0-b55b-56c5f19cf76f';
+export const MODAL_CLOSE_SEC = 2.5;
